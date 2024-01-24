b906310ce8198b8b5aca9ca545c7a5412363449e
IamHuadong <1761339618@qq.com>
2024-01-24 23:04:41 +0800

    fix: setting

--- src/export/Setting/index.tsx
+++ src/export/Setting/index.tsx
@@ -6,12 +6,13 @@ export type { IFormValues } from './default'
 export { defaultFormValues } from './default'
 
 interface ISettingProps {
+	initialValues: FormProps['initialValues']
 	onValuesChange: FormProps['onValuesChange']
 }
 
-export const Setting: React.FC<ISettingProps> = ({ onValuesChange }) => {
+export const Setting: React.FC<ISettingProps> = ({ onValuesChange, initialValues }) => {
 	return (
-		<Form onValuesChange={onValuesChange}>
+		<Form onValuesChange={onValuesChange} initialValues={initialValues}>
 			<Form.Item label='按钮文本' name='text' >
 				<Input/>
 			</Form.Item>
